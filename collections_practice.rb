780d670d34dbabc2ac0c4f91d3f93008cbb65567
Nick Bazaldua <bazalnick@gmail.com>
2020-08-02 14:42:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,5 +52,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| element + "s" if i < 1 || i > 1}
+  array.each_with_index.collect{|element, index| element + "s" if index < 1 || index > 1}
 end
